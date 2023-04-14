f65d544f9a1464b819a84c6ab3c4d17fd677cb68
Muneeb Ur Rehman <muneeburrehman250@gmail.com>
2023-04-14 23:31:36 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+
+  return (
+    <div className='flex flex-col h-[100vh] bg-blue-100 justify-center items-center'>
+      <p className='text-3xl'>👋</p>
+      <h1 className='text-5xl font-black p-3 text-blue-500 bg-blue-200  rounded-lg text-center my-2'>Welcome to React + Hardhat + Tailwindcss!</h1>
+    </div>
+  )
+}
+
+export default App
